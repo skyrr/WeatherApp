d5f30b09f1062807bebbd7b3c53c6538ad040f1e
vnebo <volodymyr.neborachko@zealicsolutions.com>
2018-09-30 19:49:50 +0300

    creating api call

--- WeatherApp/Controllers/ValuesController.cs
+++ WeatherApp/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace WeatherApp.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "" + id;
         }
 
         // POST api/values
---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Weather")]
+    public class WeatherController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("forecast/{city}")]
+        public IActionResult Forecast(string city)
+        {
+            var weather = city;
+            return Json(weather);
+        }
+
+    }
+}
