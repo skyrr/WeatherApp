371bd45b213a80b0707be3ca3dfb5689bda53991
vnebo <volodymyr.neborachko@zealicsolutions.com>
2018-10-02 12:11:20 +0300

    added apiKey to config

--- WeatherApp/Controllers/WeatherController.cs
+++ WeatherApp/Controllers/WeatherController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using WeatherApp.Models;
 
@@ -15,11 +16,18 @@ namespace WeatherApp.Controllers
     [Route("api/Weather")]
     public class WeatherController : Controller
     {
+        public static IConfigurationRoot Configuration;
         // GET api/values/5
         [HttpGet("forecast/{city}")]
         public IActionResult ForecastByCity(string city, string searchType)
         {
-            string apiKey = "fcadd28326c90c3262054e0e6ca599cd";
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+            var apiKey = Configuration["apiKey"];
+            //string apiKey = "fcadd28326c90c3262054e0e6ca599cd";
             string cityCodeLeipzig = "6548737";
             HttpWebRequest apiRequest = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?id=" + city + "&appid=" + apiKey + "&units=metric") as HttpWebRequest;
 
