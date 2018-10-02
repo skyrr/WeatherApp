cbb4d955295e7fe4e12448ed3127434c4630dbbb
vnebo <volodymyr.neborachko@zealicsolutions.com>
2018-10-02 07:04:53 +0300

    backend works

--- WeatherApp/Controllers/WeatherController.cs
+++ WeatherApp/Controllers/WeatherController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WeatherApp.Models;
 
 namespace WeatherApp.Controllers
 {
@@ -13,11 +17,21 @@ namespace WeatherApp.Controllers
     {
         // GET api/values/5
         [HttpGet("forecast/{city}")]
-        public IActionResult Forecast(string city)
+        public IActionResult ForecastByCity(string city, string searchType)
         {
-            var weather = city;
-            return Json(weather);
-        }
+            string apiKey = "fcadd28326c90c3262054e0e6ca599cd";
+            string cityCodeLeipzig = "6548737";
+            HttpWebRequest apiRequest = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?id=" + city + "&appid=" + apiKey + "&units=metric") as HttpWebRequest;
 
+            string apiResponse = "";
+            using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                apiResponse = reader.ReadToEnd();
+            }
+            
+            return Content(apiResponse);
+        }
+        // GET api/values/5
     }
 }
